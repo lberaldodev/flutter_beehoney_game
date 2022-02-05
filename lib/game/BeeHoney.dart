80e9218da6361df3215cbb71014975b966c56754
Lucas Beraldo <lucas.beraldo@dextra-sw.com>
2022-02-04 23:23:21 -0300

    feat: Load objects and add to each one hitBox

--- lib/game/BeeHoney.dart
+++ lib/game/BeeHoney.dart
@@ -1,10 +1,18 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/geometry.dart';
+import 'package:flame/input.dart';
 import 'package:flutter_beehoney_game/shared/BackgroundObject.dart';
+import 'package:flutter_beehoney_game/shared/BeeObject.dart';
+import 'package:flutter_beehoney_game/shared/FlowerObject.dart';
+import 'package:flutter_beehoney_game/shared/SpiderObject.dart';
 
-class BeeHoney extends FlameGame {
+class BeeHoney extends FlameGame with KeyboardEvents, HasCollidables {
   Bg bg = Bg();
   Bg bg2 = Bg();
+  Bee bee = Bee();
+  Spider spider = Spider();
+  Flower flower = Flower();
 
   @override
   Future<void>? onLoad() async {
@@ -14,6 +22,7 @@ class BeeHoney extends FlameGame {
       ..size.y = 900
       ..position = Vector2(0, 0);
 
+    //bg to make infinity bg
     bg2
       ..sprite = await Sprite.load('bg.png')
       ..size.x = 500
@@ -23,6 +32,31 @@ class BeeHoney extends FlameGame {
     add(bg2);
     add(bg);
 
+    bee
+      ..sprite = await Sprite.load('bee1.png')
+      ..size = Vector2.all(50)
+      ..position = Vector2(250, 700)
+      ..anchor = Anchor.center
+      ..addHitbox(HitboxRectangle());
+
+    add(bee);
+
+    spider
+      ..sprite = await Sprite.load('spider1.png')
+      ..size = Vector2.all(80)
+      ..position = Vector2(250, 500)
+      ..anchor = Anchor.center
+      ..addHitbox(HitboxRectangle());
+    add(spider);
+
+    flower
+      ..sprite = await Sprite.load('florwer1.png')
+      ..size = Vector2.all(20)
+      ..position = Vector2(250, 300)
+      ..anchor = Anchor.center
+      ..addHitbox(HitboxRectangle());
+    add(flower);
+
     return super.onLoad();
   }
 
@@ -30,8 +64,6 @@ class BeeHoney extends FlameGame {
   void update(double dt) {
     bg.move(dt, 100, 900, 0);
     bg2.move(dt, 100, 0, -900);
-
     super.update(dt);
   }
-
 }
